5cc9967b7e22e598f280f85351f33c752eb5f128
apolydorides <andreas.polydorides@gmail.com>
2018-11-13 22:32:28 +0000

    working - added sequence statistics

--- Playground.cpp
+++ Playground.cpp
@@ -5,36 +5,38 @@
 
 using namespace std;
 
-class Protein
-{
-  private:
-  int itemNum = 0;
-  int giID;
-  string refID;
-  string description;
-  string proteinSeq;
+int main() {
 
-  public:
-  readThrough();
-};
-
-class Proteins
-{
-  private:
-  vector<Protein> prots;
-
-  add_protein(Protein p){
-    prots.push_back(p);
+string str;
+str = "MAVASTSTSDGMLTLDLIQEEDPSPEEPTSCAESFRVDLDKSVAQLAGSRRRADSDRIQPSADRASSLSRPWEKTDKGATYTPQAPKKLTPTEKGRCASLEEILSQRDAASARTLQLRAEEPPTPALPNPGQLSRIQDLVARKLEETQELLAEVQGLGDGKRKAKDPPRSPPDSESEQLLLETERLLGEASSNWSQAKRVLQEVRELRDLYRQMDLQTPDSHLRQTTPHSQYRKSLM";
+int counters[26] = {};
+for (char& c : str) {
+  if ((c-int('A')) <= 9) {
+    counters[c - int('A')]++;
   }
-
-  public:
-  find_name(string name) {
-    for(Protein prot : prots){
-      if(prot.name == name) {
-        return prot
-        }
-    }   
+  else if (((c-int('A')) > 9) && ((c-int('A')) <= 14)) {
+    counters[c - int('A') - 1]++;
   }
-};
+  else if (((c-int('A')) > 14) && ((c-int('A')) < 26)) {
+    counters[c - int('A') - 2]++;
+  }
+  else if (int(c) == 42) {
+    counters[24]++;
+  }
+  else if (int(c) == 45) {
+    counters[25]++;
+  }
+}
+
+for (int j = 0 ; j < 13 ; j++) {
+  if (j < 9) { //for printing the two columns A-I and P-Y
+  cout << char(int('A')+j) << ((counters[j] > 9) ? "  " : "   ") << counters[j] << "    " << char(int('P')+j) << ((counters[j+13] > 9) ? "  " : "   ") << counters[j+13] << endl;
+  }
+  else {
+  cout << char(int('A')+j+1) << ((counters[j] > 9) ? "  " : "   ") << counters[j] << "    " << ((j < 11) ? char(int('P')+j) : char(42+3*(j-11))) << ((counters[j+13] > 9) ? "  " : "   ") << counters[j+13] << endl;
+  }
+}
 
 
+return 0;
+}
--- readingProt.cpp
+++ readingProt.cpp
@@ -57,6 +57,11 @@ public:
     return descr;
   };
 
+  string get_sequence()
+  {
+    return sequence;
+  };
+
   bool keywordMatch(string str)
   {
     if (descr.find(str) != string::npos)
@@ -67,7 +72,11 @@ public:
   };
 };
 
+
+void fileMenu();
 void dbMenu(vector<Protein> proteins);
+void protMenu(Protein p);
+
 
 /*Tokenizing the proteins by finding the ">" character
 Then separating the first line using the "|" character
@@ -168,7 +177,7 @@ void dbMenu(vector<Protein> proteins) //Database Menu
          << "6) Quit database" << endl;
     cin >> selection;
     badInput = cin.fail();
-    if (badInput || selection < 1 || selection > 3)
+    if (badInput || selection < 1 || selection > 6)
     {
       cin.clear();
       cin.ignore(10, '\n');
@@ -178,121 +187,187 @@ void dbMenu(vector<Protein> proteins) //Database Menu
 
   int idSearch;
   string search;
-  switch (selection)
-  {
-  case 1:
-    cout << endl
-         << "The proteins in the database are from GenBank(R)." << endl
-         << "Total number of proteins in this database: " << proteins.size() << endl//Add proteins.size()?
-         << "Amino acids are represented by the following characters:" << endl
-         << "A  alanine                P  proline" << endl
-         << "B  aspartate/asparagine   Q  glutamine" << endl
-         << "C  cystine                R  arginine" << endl
-         << "D  aspartate              S  serine" << endl
-         << "E  glutamate              T  threonine" << endl
-         << "F  phenylalanine          U  selenocysteine" << endl
-         << "G  glycine                V  valine" << endl
-         << "H  histidine              W  tryptophan" << endl
-         << "I  isoleucine             Y  tyrosine" << endl
-         << "K  lysine                 Z  glutamate/glutamine" << endl
-         << "L  leucine                X  any" << endl
-         << "M  methionine             *  translation stop" << endl
-         << "N  asparagine             -  gap of indeterminate length" << endl ;
-    dbMenu(proteins);
-    break;
-  case 2:
-    cout << "Enter a protein number:" << endl;
-    cin >> idSearch;
-    for (Protein p : proteins)
-    {
-      if (p.get_id() == idSearch)
-      {
-        cout << p << endl;
-        break;
-      }
-      else if (p.get_id() == proteins.size())
-      {
-        cout << "Your search yielded no results. Please check your input is correct and try again." << endl;
-        dbMenu(proteins);
-      }
-    }
-    break;
-  case 3:
-    cout << "Enter a GI number:" << endl;
-    cin >> search;
-    for (Protein p : proteins)
-    {
-      if (p.get_gi() == search)
-      {
-        cout << p << endl;
-        break;
-      }
-      else
-      {
-        cout << "Your search yielded no results. Please check your input is correct and try again." << endl;
-        dbMenu(proteins);
-      }
-    }
-    break;
-  case 4:
-    cout << "Enter a reference number:" << endl;
-    cin >> search;
-    for (Protein p : proteins)
-    {
-      if (p.get_ref() == search)
-      {
-        cout << p << endl;
-        //break; //since ref,gi,id are specific to 1 protein we break to reduce run-time
-      }
-      else
-      {
-        cout << "Your search yielded no results. Please check your input is correct and try again." << endl;
-        dbMenu(proteins);
-      }
-    }
-    break;
-  case 5:
-    cout << "Enter a keyword to search for:" << endl;
-    cin >> search;
-    int i=0;
-    for(Protein p : Protein) {
-      if(p.keywordMatch(search)) {
-        cout << ++i << ") " << p << endl;
-      }
-    }
-    if (i == 0) {
-      cout << "Your search yielded no results. Please check your input is correct and try again." << endl;
+  int i = 0;
+  switch (selection) {
+    case 1:
+      cout << endl
+          << "The proteins in the database are from GenBank(R)." << endl
+          << "Total number of proteins in this database: " << proteins.size() << endl//Add proteins.size()?
+          << "Amino acids are represented by the following characters:" << endl
+          << "A  alanine                P  proline" << endl
+          << "B  aspartate/asparagine   Q  glutamine" << endl
+          << "C  cystine                R  arginine" << endl
+          << "D  aspartate              S  serine" << endl
+          << "E  glutamate              T  threonine" << endl
+          << "F  phenylalanine          U  selenocysteine" << endl
+          << "G  glycine                V  valine" << endl
+          << "H  histidine              W  tryptophan" << endl
+          << "I  isoleucine             Y  tyrosine" << endl
+          << "K  lysine                 Z  glutamate/glutamine" << endl
+          << "L  leucine                X  any" << endl
+          << "M  methionine             *  translation stop" << endl
+          << "N  asparagine             -  gap of indeterminate length" << endl ;
       dbMenu(proteins);
-    }
-    break;
-  default:
-    cout << "Exiting database";
-    exit(0); //since I am only allowing values 1-6 to be handled by the switch statement, default (i.e. 6) will exit the database
-    break;
+      break;
+    case 2:
+      cout << "Enter a protein number:" << endl;
+      cin >> idSearch;
+      for (Protein p : proteins)
+      {
+        if (p.get_id() == idSearch)
+        {
+          cout << endl << "You selected:" << endl;
+          cout << p.get_descr() << endl;
+          protMenu(p);
+          break;
+        }
+        else if (p.get_id() == proteins.size())
+        {
+          cout << "Your search yielded no results. Please check your input is correct and try again." << endl;
+          dbMenu(proteins);
+        }
+      }
+      break;
+    case 3:
+      cout << "Enter a GI number:" << endl;
+      cin >> search;
+      for (Protein p : proteins)
+      {
+        if (p.get_gi() == search)
+        {
+          cout << endl << "You selected:" << endl;
+          cout << p.get_descr() << endl;
+          protMenu(p);
+          break;
+        }
+        else
+        {
+          cout << "Your search yielded no results. Please check your input is correct and try again." << endl;
+          dbMenu(proteins);
+        }
+      }
+      break;
+    case 4:
+      cout << "Enter a reference number:" << endl;
+      cin >> search;
+      for (Protein p : proteins)
+      {
+        if (p.get_ref() == search)
+        {
+          cout << endl << "You selected:" << endl;
+          cout << p.get_descr() << endl;
+          protMenu(p);
+          break; //since ref,gi,id are specific to 1 protein we break to reduce run-time
+        }
+        else
+        {
+          cout << "Your search yielded no results. Please check your input is correct and try again." << endl;
+          dbMenu(proteins);
+        }
+      }
+      break;
+    case 5:
+      cout << "Enter a keyword to search for:" << endl;
+      cin >> search;
+      for(Protein p : proteins) {
+        if(p.keywordMatch(search)) {
+          cout << ++i << ") " << p << endl << endl;
+        }
+        else if (p.get_id()==proteins.size() && (i == 0)) {
+          cout << "Your search yielded no results. Please check your input is correct and try again." << endl;
+          dbMenu(proteins);
+        }
+      }
+      break;
+    default:
+      cout << "Exiting database";
+      exit(0); //since I am only allowing values 1-6 to be handled by the switch statement, default (i.e. 6) will exit the database
+      break;
   }
 }
 
+void protMenu(Protein p) {
+  int selection = 0;
+  bool badInput = false;
+  while (selection < 1 || selection > 5 || badInput)
+  {
+    badInput = false; //resets bad input
+    cout << endl
+         << "Select an option from the menu below:" << endl
+         << "1) Description of the protein" << endl
+         << "2) Protein sequence" << endl
+         << "3) Protein statistics" << endl
+         << "4) Record protein to file" << endl
+         << "5) Return to main menu" << endl;
+    cin >> selection;
+    badInput = cin.fail();
+    if (badInput || selection < 1 || selection > 5)
+    {
+      cin.clear();
+      cin.ignore(10, '\n');
+      cout << "Invalid input. Please enter a value between 1 and 6.";
+    }
+  }
+
+  int counters[26] = {};
+  switch (selection) {
+    case 1:
+      cout << p << endl;
+      protMenu(p);
+      break;
+    case 2:
+      cout << endl << "Protein sequence:" << endl;
+      cout << p.get_sequence() << endl;
+      protMenu(p);
+      break;
+    case 3:
+      for (char& c : p.get_sequence()) {
+        if ((c-int('A')) <= 9) {
+          counters[c - int('A')]++;
+        }
+        else if (((c-int('A')) > 9) && ((c-int('A')) <= 14)) {
+          counters[c - int('A') - 1]++;
+        }
+        else if (((c-int('A')) > 14) && ((c-int('A')) < 26)) {
+          counters[c - int('A') - 2]++;
+        }
+        else if (int(c) == 42) {
+          counters[24]++;
+        }
+        else if (int(c) == 45) {
+          counters[25]++;
+        }
+      }
+      cout << endl << "Protein statistics:" << endl;
+      for (int j = 0 ; j < 13 ; j++) {
+        if (j < 9) { //for printing the two columns A-I and P-Y
+        cout << char(int('A')+j) << ((counters[j] > 9) ? "  " : "   ") << counters[j] << "    " << char(int('P')+j) << ((counters[j+13] > 9) ? "  " : "   ") << counters[j+13] << endl;
+        }
+        else {//prints the remainders of the two columns
+        cout << char(int('A')+j+1) << ((counters[j] > 9) ? "  " : "   ") << counters[j] << "    " << ((j < 11) ? char(int('P')+j) : char(42+3*(j-11))) << ((counters[j+13] > 9) ? "  " : "   ") << counters[j+13] << endl;
+        }
+      }
+      protMenu(p);
+      break;
+    case 4:
+      cout << "Not available yet.";
+      break;
+    default:
+      cout << "Not available yet.";
+      break;
+  }
+
+}
+
+
+
+
 int main()
 {
+  system("CLS");
   cout << endl << "Welcome to the Protein Database" << endl;
 
   fileMenu();
-
-  /*
-  for(Protein p : Protein) {
-    if (p.get_ref()=="XP_005261862.1")  {
-      cout << p << endl;
-      break; //since ref,gi,id are specific to 1 protein we break to reduce run-time
-    }
-  }
-
-  int i = 0;
-  for(Protein p : Protein) {
-    if(p.keywordMatch("olf")) {
-      cout << ++i << ") " << p << endl;
-    }
-  }
-*/
 
   return 0;
 }
